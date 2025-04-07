489abb9d07164a8422dd69fb4de62a82448bf2a7
Zachar Čuřík <148170722+zacharcc@users.noreply.github.com>
2025-04-07 04:06:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 # --- Paths ---
 SANDWORM_REPO="$HOME/Sandworm/test"
-CONFIG_DIR="$HOME/printer_data/config/TEST/update_test/"
-MOONRAKER_CONF="$CONFIG_DIR/moonraker.conf"
+CONFIG_DIR="$HOME/printer_data/config/TEST/update_test"
+MOONRAKER_CONF="$HOME/printer_data/config/moonraker.conf"
 BACKUP_DIR="$HOME/Sandworm/Backup/backup_config_$(date +%Y%m%d_%H%M%S)"
 LOGFILE="$HOME/Sandworm/update_logs/update_$(date +%Y%m%d_%H%M%S).log"
 

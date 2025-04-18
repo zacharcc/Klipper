9e26eb85cf15d4051188551006aefc026498abcf
Zachar Čuřík <148170722+zacharcc@users.noreply.github.com>
2025-04-18 22:51:56 +0200

    Add files via upload

--- tools/game_intro.sh
+++ tools/game_intro.sh
@@ -91,11 +91,11 @@ echo -e "${F3}██████████████████████
 sleep $SPEED_MEDIUM
 echo -e "${F3}█${C0}${BG1}                                                                                               ${C0}${F3}█${C0}"
 sleep $SPEED_MEDIUM
-echo -e "${F3}█${C0}${BG1}   ▓ Game Save: ${game_save}               ▓ ARMOR: Chamber           ▓ STAMINA: 40% (Of max flow)     ${C0}${F3}█${C0}"
+echo -e "${F3}█${C0}${BG1}   ▓ Game Save: ${game_save}                ▓ ARMOR: Chamber           ▓ STAMINA: 40% (Of max flow)     ${C0}${F3}█${C0}"
 sleep $SPEED_MEDIUM
 echo -e "${F3}█${C0}${BG1}                                                                                               ${C0}${F3}█${C0}"
 sleep $SPEED_MEDIUM
-echo -e "${F3}█${C0}${BG1}   ▓ LEVEL: ${level} Unlocked                                       ▓ AMMO: 3xASA, 2xPLA             ${C0}${F3}█${C0}"
+echo -e "${F3}█${C0}${BG1}   ▓ LEVEL: ${level} Unlocked                                      ▓ AMMO: 3xASA, 2xPLA             ${C0}${F3}█${C0}"
 sleep $SPEED_MEDIUM
 echo -e "${F3}█${C0}${BG1}                                                                                               ${C0}${F3}█${C0}"
 sleep $SPEED_MEDIUM

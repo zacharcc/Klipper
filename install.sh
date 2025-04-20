adc0f636a031400ebc5f64841f8eb09669ad66b5
Zachar Čuřík <148170722+zacharcc@users.noreply.github.com>
2025-04-20 02:42:28 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -208,8 +208,8 @@ restart_moonraker() {
 
     else
 	    echo ""
-        echo -e "$INFO Moonraker restart skipped. Changes have not been applied,"
-		echo -e "but you can restart Moonraker manually later via:"
+        echo -e "$INFO Moonraker restart skipped. Changes have not been applied!"
+		echo -e "But you can restart Moonraker manually later via:"
         echo -e "  1. The web interface: Power -→ Service Control -→ Moonraker"
         echo -e "  2. Command line: curl -X POST http://localhost:7125/server/restart"
     fi

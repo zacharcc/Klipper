255588a27712b72453719052184b3348bea162fe
zacharcc <zachar.c@gmail.com>
2025-04-22 14:43:17 +0200

    Update install.sh and game into

--- install.sh
+++ install.sh
@@ -205,7 +205,7 @@ copy_files() {
     echo ""
     mkdir -p "$CONFIG_DIR"
     rsync -av "$SANDWORM_REPO/" "$CONFIG_DIR/"
-    sleep $MESS_sDELAY
+    sleep 0.5
     echo ""
     
     echo "$OK Copying completed."

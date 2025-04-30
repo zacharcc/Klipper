100f0ea7f67886e0cce7d0d6b7734b1b2630be40
zacharcc <zachar.c@gmail.com>
2025-05-01 01:14:08 +0200

    Test uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -145,7 +145,7 @@ print_row "$OK Sandworm macros have been uninstalled successfully!"
 print_row ""
 print_row "To apply the changes, please restart Moonraker using the [Y] option."
 print_row "Uninstall log saved to:"
-print_row "   ● $LOGFILE  "
+echo -e "║   ● $LOGFILE       ║"
 echo "╚═════════════════════════════════════════════════════════════════════════════════╝"
 
 # --- Optional [y/N]: Restart Moonraker ---
